7097e681795e9488cda56b54d0c83825333412b7
Stuart Williams <spookdejur@hotmail.com>
2025-09-06 09:55:39 -0700

    Cleanup stray files

--- data/salesdb/cvrt.ps1
+++
@@ -1,6 +0,0 @@
-<#
-    Convert-to-Quoted-Delimited
-#>
-$filein = 'f100.csv'
-$fileout = 'f100-PS.csv'
-import-csv -LiteralPath $filein | export-csv -LiteralPath $fileout -NoTypeInformation -Encoding UTF8 -QuoteFields Company,Ticker
